1f542d3d7d51519bae2783b1fcfc28a0706bee71
KostyaF1 <k_moroz@hotmail.com>
2018-12-18 11:02:02 +0200

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:8
+Run mkdir express_app
+WORKDIR /express_app
+COPY package.json /express_app
+RUN npm install
+COPY . /express_app
+CMD node app.js
+EXPOSE 3000
+
+
+
--- app.js
+++ app.js
@@ -5,6 +5,16 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+//app.set('port', process.env.PORT || 8088);
+
+//app.get('/', function(req, res){
+//    res.send('hello world');
+//});
+
+//app.listen(app.get('port'));
