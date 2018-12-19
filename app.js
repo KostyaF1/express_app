a55cc9e80c139525148b8e948572a7c3b97c83d0
KostyaF1 <k_moroz@hotmail.com>
2018-12-19 13:46:00 +0200

    ver 19.8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Halleeeeee==)))))!!!,  Devops!!!!');
+  res.send('Halleeeeeee==)))))!!!,  Devops!!!!');
 });
 
 
