0d48bad2661e7104ca4ee2973e514e393c48869e
KostyaF1 <k_moroz@hotmail.com>
2018-12-20 16:40:35 +0200

    ves 25

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Halleeeeeee=)))))!!!,  Devops!!!!');
+  res.send('Hall0000=)))))!!!,  Devops!!!!');
 });
 
 
