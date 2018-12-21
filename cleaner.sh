a3d33afd33aaffb893251a4910d5874c84d239f5
KostyaF1 <k_moroz@hotmail.com>
2018-12-21 10:58:40 +0200

    add comments to script file

--- cleaner.sh
+++ cleaner.sh
@@ -1,9 +1,12 @@
 #!bin/bash
 
+#Export global variable.
 export IMAGE_NAME
 
+#Get list of images
 CONTAINER_IDs=$(docker images -q $IMAGE_NAME)
 
+#If there are more then 0 images - remove them
 if [ "$CONTAINER_IDs" != "" ]
   then
     docker rmi $CONTAINER_IDs
